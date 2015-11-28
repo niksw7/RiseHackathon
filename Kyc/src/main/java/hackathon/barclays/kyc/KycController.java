22d9ad6a31a109e26bc3a45f1572493ee493b328
nikesh <niksw7@gmail.com>
2015-11-28 18:44:58 +0530

    Skeleton for api
    Skeleton for api

---
+++ Kyc/src/main/java/hackathon/barclays/kyc/KycController.java
@@ -0,0 +1,17 @@
+package hackathon.barclays.kyc;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KycController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
