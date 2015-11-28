f4eff4095ca50516f96b44db7f53b77432a2dde2
Mukesh kumar Gupta <mkshkmrgpt@gmail.com>
2015-11-29 01:00:38 +0530

    test

--- Kyc/src/main/java/hackathon/barclays/kyc/model/KYCDetails.java
+++ Kyc/src/main/java/hackathon/barclays/kyc/model/KYCDetails.java
@@ -5,5 +5,23 @@ package hackathon.barclays.kyc.model;
 public class KYCDetails {
     private String name;
     private String age;
-    private String documents;
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getDocuments() {
+		return documents;
+	}
+	public void setDocuments(String documents) {
+		this.documents = documents;
+	}
+	private String documents;
 }
